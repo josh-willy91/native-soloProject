c6510ec5e4781af64c24ba67264f2c494397e413
josh-willy91 <jwilly91812@yahoo.com>
2021-06-23 21:07:44 -0400

    finished for weds night

--- frontend/src/components/EventDetails/index.js
+++ frontend/src/components/EventDetails/index.js
@@ -1,14 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
 import { oneEvent } from '../../store/events';
+import formatISO from 'date-fns/formatISO';
 
 const EventDetails = () => {
     // declare variables from hooks
     const dispatch = useDispatch();
     const eventState = useSelector((state) => state.events);
     const event = eventState.oneEvent;
+    // ('+020201-06-26T00:00:00.000Z')
+
+    // formatISO(date, [options]) syntax for function
+    // formats the data string returned from query
+    let isoFormat = function(dateString) {
+        const result = formatISO(new Date(dateString), { representation: 'date' })
+        return result;
+    };
 
     // Get the id with useParams hook and pass into the thunk
     // on line 19 when I make a call to dispatch the store
@@ -27,10 +35,19 @@ const EventDetails = () => {
         <>
             <div>
                 <h1>{event && event[0].name}</h1>
-                <h3>{event && event[0].hostId}</h3>
+                <h3>Hosted by user {event && event[0].User.username}</h3>
+                {/* {create NavLink to link to user profile} */}
+                <h3>{event && isoFormat(event[0].date)}</h3>
             </div>
             <div>
-                {event && event[0].name}
+                <img src="/images/defaultImage.jpg"/>
+            </div>
+            <div>
+                <p>{event && event[0].time}</p>
+                <p>{event && isoFormat(event[0].date)}</p>
+            </div>
+            <div>
+                {/* <p>{add a details field for seeder and migration}</p> */}
             </div>
         </>
     )
