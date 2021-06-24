ec6e9d25828c5c451b1b1686f40f725db5df9707
josh-willy91 <jwilly91812@yahoo.com>
2021-06-24 13:43:25 -0400

    added Venue to my include query for eventDetails

--- backend/routes/api/eventDetails.js
+++ backend/routes/api/eventDetails.js
@@ -6,12 +6,18 @@ const router = express.Router();
 // import Event model to run queries
 const { Event } = require('../../db/models');
 const { User } = require('../../db/models');
+const { Venue } = require('../../db/models');
+const { Rsvp } = require('../../db/models');
 
 
 router.get('/:id', asyncHandler(async(req, res) => {
     const {id} = req.params;
     const event = await Event.findByPk(id, {
-        include: User
+        include: [User, Venue]
+        // include: [{model: User, model: Venue}]
+        // include: [User, Venue]
+        // include: [{User, Venue}]
+        // include: User, Venue
     });
     res.json(event);
 }));
