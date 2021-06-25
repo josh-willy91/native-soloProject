1e01c1d664663c28dd863e31e345f8ed1219b7d9
josh-willy91 <jwilly91812@yahoo.com>
2021-06-25 14:04:13 -0400

    action creators

--- frontend/src/store/userProfile.js
+++ frontend/src/store/userProfile.js
@@ -1,8 +1,10 @@
 import { csrfFetch } from './csrf';
 
+// define action types as constants
 const GET_USER = 'profile/GET_USER';
 // const DELETE_USER = 'profile/DELETE_USER';
 
+//// define action creators
 const getUser = (id) => ({
     type: GET_USER,
     payload: id,
