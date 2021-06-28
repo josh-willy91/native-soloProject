df660be866a00c567ecbac36d8dc258335d3d647
josh-willy91 <jwilly91812@yahoo.com>
2021-06-27 22:58:25 -0400

    finished crud for events

--- backend/routes/api/createForm.js
+++ backend/routes/api/createForm.js
@@ -8,26 +8,59 @@ const { Event } = require('../../db/models')
 const { User } = require('../../db/models')
 const { Group } = require('../../db/models')
 const { Rsvp } = require('../../db/models')
+const { Venue } = require('../../db/models')
 
 router.post('/event', asyncHandler(async(req, res) => {
-    console.log(req, '============================')
-    const username = req.body.username;
-    const email = req.body.payload;
-    const password = req.body;
-    console.log(username, email, password, '============================')
+    const event = req.body.event;
+    const venue = req.body.venue;
+    const group = req.body.group;
 
-    // const newEvent = await Event.create({
-    //     username: username,
-    //     email: email,
-    //     password: password,
-    // });
+    const venueName = venue.venueName;
+    const address = venue.address;
+    const city = venue.city;
+    const state = venue.state;
+    const zipCode = venue.zipCode;
+    const lat = venue.lat;
+    const lon = venue.lon;
+
+    const newVenue = await Venue.create({
+        name: venueName,
+        address,
+        city,
+        state,
+        zipCode,
+        lat,
+        lon,
+    });
+
+    const type = group.type;
+
+    const newGroup = await Group.create({
+        type,
+    });
+
+    const hostId = event.hostId;
+    const name = event.name;
+    const details = event.details;
+    const date = event.date;
+    const time = event.time;
+    const capacity = event.capacity;
+
+    const newEvent = await Event.create({
+        hostId,
+        venueId: newVenue.id,
+        categoryId: newGroup.id,
+        name,
+        details,
+        date,
+        time,
+        capacity,
+    })
+
+    console.log(hostId, event, '=================')
 
     res.json({'test': 'test'});
 }));
 
-
-// router.post('/group', asyncHandler(async(req, res) => {
-
-// }));
 
 module.exports = router;
